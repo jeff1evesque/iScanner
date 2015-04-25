ed9dbef74b4ada85ae5da07e1818471df97961b9
jeffrey levesque <jeff1evesque@yahoo.com>
2015-04-25 15:18:54 -0400

    #1: default.pp, install c++ compiler

--- manifests/default.pp
+++ manifests/default.pp
@@ -13,6 +13,14 @@ class gnome {
     }
 }
 
+# gcc: installs the c++ compiler if not 'present'
+class gcc {
+    package {'g++':
+        ensure => present,
+    }
+}
+
 # implement classes
 include git
 include gnome
+include gcc

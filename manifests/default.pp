b016ba701d5030081c8fb0fdd1d45323c798e03f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-26 10:04:38 -0400

    #3: default.pp, add 'OpenCV' directory

--- manifests/default.pp
+++ manifests/default.pp
@@ -7,7 +7,7 @@ package {'git':
 }
 
 ## directory: create 'opencv' directory
-file {"${opencv_directory}/opencv":
+file {"${opencv_directory}/OpenCV":
     ensure => 'directory',
 }
 
@@ -26,14 +26,14 @@ file {"${opencv_directory}/opencv":
 exec {'git-opencv':
     command => 'git clone https://github.com/Itseez/opencv.git opencv/',
     require => [Package['git'], File["${opencv_directory}/opencv"]],
-    cwd     => "${opencv_directory}/opencv",
+    cwd     => "${opencv_directory}/OpenCV",
     path    => '/usr/bin',
     timeout => 450,
     notify  => Exec['cmake-opencv'],
 }
 
 ## directory: create 'release' directory
-file {"${opencv_directory}/opencv/release":
+file {"${opencv_directory}/OpenCV/opencv/release":
     ensure => 'directory',
 }
 
@@ -44,6 +44,6 @@ file {"${opencv_directory}/opencv/release":
 #      or ~>.  In this case, 'git-opencv' implements the needed notify event.
 exec {'cmake-opencv':
     command     => 'cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..'
-    cwd         => "${opencv_directory}/opencv/release",
+    cwd         => "${opencv_directory}/OpenCV/opencv/release",
     refreshonly => true,
 }

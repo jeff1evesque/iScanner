84694d2057c85fe20e08a532c16cebf8595f929e
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-26 09:54:14 -0400

    #3: default.pp, define 'cmake-opencv', and notify-refresh relationship

--- manifests/default.pp
+++ manifests/default.pp
@@ -29,9 +29,21 @@ exec {'git-opencv':
     cwd     => "${opencv_directory}/opencv",
     path    => '/usr/bin',
     timeout => 450,
+    notify  => Exec['cmake-opencv'],
 }
 
 ## directory: create 'release' directory
 file {"${opencv_directory}/opencv/release":
     ensure => 'directory',
+}
+
+## cmake-opencv:
+#
+#  @refreshonly, when set to true, the corresponding exec will only run when
+#      it receives an event. Refresh events can be sent via notify, subscribe,
+#      or ~>.  In this case, 'git-opencv' implements the needed notify event.
+exec {'cmake-opencv':
+    command     => 'cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..'
+    cwd         => "${opencv_directory}/opencv",
+    refreshonly => true,
 }

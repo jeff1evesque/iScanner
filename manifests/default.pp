de92a88de2b8aa5da9be0cbd0849aaa3265b4fee
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-26 19:05:33 -0400

    #3: default.pp, redefine 'opencv_codebase', and 'unzip'

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,5 @@
 ## variables
-$opencv_codebase   = https://github.com/Itseez/opencv/archive/3.0.0-beta.zip
+$opencv_codebase   = 'https://github.com/Itseez/opencv/archive/3.0.0-beta.zip'
 $opencv_directory  = '/home/vagrant'
 $opencv_dependency = ['libopencv-dev', 'build-essential', 'checkinstall', 'cmake', 'pkg-config', 'yasm', 'libtiff4-dev', 'libjpeg-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libdc1394-22-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'python-dev', 'python-numpy', 'libtbb-dev', 'libqt4-dev', 'libgtk2.0-dev', 'libfaac-dev', 'libmp3lame-dev', 'libopencore-amrnb-dev', 'libopencore-amrwb-dev', 'libtheora-dev', 'libvorbis-dev', 'libxvidcore-dev', 'x264', 'v4l-utils', 'unzip']
 
@@ -57,7 +57,7 @@ exec {'wget-opencv':
 
 ## unzip-opencv: unzip the installed opencv.
 exec {'unzip-opencv':
-    command => 'unzip opencv',
+    command => 'unzip opencv.zip',
     cwd     => "${opencv_directory}",
     refreshonly => true,
 }

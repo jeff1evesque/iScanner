231db8ea28b97357edfb860bd5ebbf493152c6f6
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-26 12:33:41 -0400

    #3: default.pp, install opencv package dependencies

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,6 @@
 ## variables
 $opencv_directory = '/home/vagrant'
+$opencv_dependecy = ["libopencv-dev", "build-essential", "checkinstall", "cmake", "pkg-config", "yasm", "libtiff4-dev", "libjpeg-dev", "libjasper-dev", "libavcodec-dev", "libavformat-dev", "libswscale-dev", "libdc1394-22-dev", "libxine-dev", "libgstreamer0.10-dev", "libgstreamer-plugins-base0.10-dev", "libv4l-dev", "python-dev", "python-numpy", "libtbb-dev", "libqt4-dev", "libgtk2.0-dev", "libfaac-dev", "libmp3lame-dev", "libopencore-amrnb-dev", "libopencore-amrwb-dev", "libtheora-dev", "libvorbis-dev", "libxvidcore-dev", "x264", "v4l-utils", 'ffmpeg']
 
 ## define $PATH for all execs
 Exec {path => ['/usr/bin/']}
@@ -9,8 +10,8 @@ package {'git':
     ensure => present,
 }
 
-## cmake: install cmake if not present
-package {'cmake':
+## opencv-dependencies
+package {$opencv_dependency:
     ensure => present,
 }
 

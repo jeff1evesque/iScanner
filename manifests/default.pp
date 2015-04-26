9e5f6ea3b874747b42aebd0ff04f5a582582775f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-26 11:13:02 -0400

    #3: default.pp, add missing comma

--- manifests/default.pp
+++ manifests/default.pp
@@ -47,7 +47,7 @@ file {"${opencv_directory}/OpenCV/opencv/release":
 #      it receives an event. Refresh events can be sent via notify, subscribe,
 #      or ~>.  In this case, 'git-opencv' implements the needed notify event.
 exec {'cmake-opencv':
-    command     => 'cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..'
+    command     => 'cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..',
     cwd         => "${opencv_directory}/OpenCV/opencv/release",
     refreshonly => true,
     notify      => Exec['make-opencv'],

8c9ae14e2d0d25e43a000f7ba95a7f159cc130c7
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-26 17:36:07 -0400

    #3: default.pp, adjust docblock

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,6 +6,8 @@ $opencv_dependency = ['libopencv-dev', 'build-essential', 'checkinstall', 'cmake
 Exec {path => ['/usr/bin/']}
 
 ## python-software-properties: installs the command 'add-apt-repository'.
+#
+#  @notify, send a 'refresh event' to 'enable-multiverse'.
 package {'python-software-properties':
     ensure => present,
     notify => Exec['enable-multiverse'],
@@ -13,6 +15,9 @@ package {'python-software-properties':
 
 ## enable-multiverse: enables multiverse repository, therefore, allows
 #                     installation of 'libfaac-dev'.
+#  @refreshonly, when set to true, the corresponding exec will only run when
+#      it receives an event. Refresh events can be sent via notify, subscribe,
+#      or ~>.  In this case, 'git-opencv' implements the needed notify event.
 exec {'enable-multiverse':
     command => 'add-apt-repository multiverse',
     require => Package['python-software-properites'],
@@ -50,7 +55,7 @@ exec {'git-opencv':
 
 ## directory: create 'release' directory
 #
-#  @notify, sends a 'refresh event' to 'cmake-opencv'.
+#  @notify, send a 'refresh event' to 'cmake-opencv'.
 file {"${opencv_directory}/opencv/release":
     ensure => 'directory',
     notify => Exec['cmake-opencv'],
@@ -58,9 +63,7 @@ file {"${opencv_directory}/opencv/release":
 
 ## cmake-opencv: build opencv
 #
-#  @refreshonly, when set to true, the corresponding exec will only run when
-#      it receives an event. Refresh events can be sent via notify, subscribe,
-#      or ~>.  In this case, 'git-opencv' implements the needed notify event.
+#  @notify, send a 'refresh event' to 'make-opencv'.
 exec {'cmake-opencv':
     command     => 'cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..',
     cwd         => "${opencv_directory}/opencv/release",

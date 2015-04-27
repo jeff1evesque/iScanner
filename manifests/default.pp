64c4674fb13b5afc439fce312cd328c0fa5b69b8
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-27 14:12:48 -0400

    #3: default.pp, define, and implement 'opencv_version'

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,6 @@
 ## variables
-$opencv_codebase   = 'https://github.com/Itseez/opencv/archive/3.0.0-beta.zip'
+$opencv_version    = '3.0.0-beta'
+$opencv_codebase   = "https://github.com/Itseez/opencv/archive/${opencv_version}.zip"
 $opencv_directory  = '/home/vagrant'
 $opencv_dependency = ['libopencv-dev', 'build-essential', 'cmake', 'git', 'libgtk2.0-dev', 'pkg-config', 'python-dev', 'python-numpy', 'libdc1394-22', 'libdc1394-22-dev', 'libjpeg-dev', 'libpng12-dev', 'libtiff4-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'libtbb-dev', 'libqt4-dev', 'libfaac-dev', 'libmp3lame-dev', 'libopencore-amrnb-dev', 'libopencore-amrwb-dev', 'libtheora-dev', 'libvorbis-dev', 'libxvidcore-dev', 'x264', 'v4l-utils', 'unzip']
 

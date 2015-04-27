c18c4c08d3ffa21e601d93987a2934fc64981e15
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-26 22:28:07 -0400

    #3: add '/bin/' to PATH variable

--- manifests/default.pp
+++ manifests/default.pp
@@ -4,7 +4,7 @@ $opencv_directory  = '/home/vagrant'
 $opencv_dependency = ['libopencv-dev', 'build-essential', 'checkinstall', 'cmake', 'pkg-config', 'yasm', 'libtiff4-dev', 'libjpeg-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libdc1394-22-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'python-dev', 'python-numpy', 'libtbb-dev', 'libqt4-dev', 'libgtk2.0-dev', 'libfaac-dev', 'libmp3lame-dev', 'libopencore-amrnb-dev', 'libopencore-amrwb-dev', 'libtheora-dev', 'libvorbis-dev', 'libxvidcore-dev', 'x264', 'v4l-utils', 'unzip']
 
 ## define $PATH for all execs
-Exec {path => ['/usr/bin/']}
+Exec {path => ['/bin/, '/usr/bin/']}
 
 ## python-software-properties: installs the command 'add-apt-repository'.
 #
